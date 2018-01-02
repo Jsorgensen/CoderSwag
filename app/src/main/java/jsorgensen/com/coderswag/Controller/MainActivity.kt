fea95f18b4a9def17151b0ed72c0eefb3701cc78
Mike Jorgensen <mikeljorgensen@gmail.com>
2018-01-02 11:48:52 -0700

    added custom adapter for list view elements

--- app/src/main/java/jsorgensen/com/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/jsorgensen/com/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package jsorgensen.com.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import jsorgensen.com.coderswag.Adapters.CategoryAdapter
 import jsorgensen.com.coderswag.Model.Category
 import jsorgensen.com.coderswag.R
 import jsorgensen.com.coderswag.Services.DataService
@@ -10,14 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
+        adapter = CategoryAdapter(this,
                 DataService.categories)
         categoryListView.adapter = adapter
     }
